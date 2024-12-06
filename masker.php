ed1c303bdef6ddcfed20b23c5e98fafcdbbc60be
tekvishal <vishal.adheli@tekditechnologies.com>
2024-12-06 18:06:00 +0530

    init

---
+++ masker.php
@@ -0,0 +1,11 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+
+class plgContentMasker extends JPlugin
+{
+    public function onContentAfterDisplay($context, &$article, &$params, $limitstart)
+    {
+        
+    }
+}
